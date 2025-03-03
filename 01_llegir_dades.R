ec8271a8ab5af355c966c51fc0a13a3b225f9e5f
fwang02 <fywang021205@gmail.com>
2025-03-03 16:58:47 +0100

    borrar fichero innecesario

--- 01_llegir_dades.R
+++
@@ -1,8 +0,0 @@
-#leer datos sin añadir NA
-data <- read.csv("reduced-data.csv",sep=";")
-head(data)
-
-#leer datos con NA
-data2 <- read.csv("data_withNA.csv", row.names = NULL)
-data2 <- data2[,-1]
-head(data2)
